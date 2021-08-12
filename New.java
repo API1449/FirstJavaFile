ff4b66ee405508750d54ea7621d531a4316efeae
Pranee-Crypto <praneetha.bapatla@apisero.com>
2021-08-12 15:16:34 +0530

    Second Commit

--- New.java
+++ New.java
@@ -3,5 +3,7 @@ public class New{
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("WELCOME!"); 
+         System.out.println("This is another line"); 
+
     }
 }
