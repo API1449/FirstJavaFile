46542fa1055281c09bf86d572310f0712b1001ed
Pranee-Crypto <praneetha.bapatla@apisero.com>
2021-08-13 10:49:54 +0530

    modified the file in api branch

--- New.java
+++ New.java
@@ -7,6 +7,11 @@ public class New{
         System.out.println("This is one more  line- dummy sentence "); 
        System.out.println("This is a line to test bit bucket");
         System.out.println("copy1"); 
+       System.out.println("Added these lines to api branch");
+       System.out.println("Learning branching in git");
+       System.out.println("will merge api branch with copy1 branch");
+       System.out.println("Finally copy1 branch will be merged to master, the root branch");
+        
 
     
     }
